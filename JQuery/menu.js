f9a025ead0fb8258c371d497c8bc81ca5785afc1
Developoz <trung_vo_6@hotmail.com>
2016-01-19 12:23:14 +0100

    changed the layout

--- JQuery/menu.js
+++ JQuery/menu.js
@@ -1,19 +1,26 @@
-$(document).ready(function(){
-	var gallery = $('.gallery');
+$(function() {
+	var images = $('.images');
+	var image = $('.images>img');
+	var current = -1;
+	var focus = $('.focus');
+	var container = $('.focus .container');
+	var close = $('.close');
 
-	var currentPictureIndex = 0;
-	$(gallery[currentPictureIndex]).show();
-
-	$('.gallery').click(function () {
-		$(gallery[currentPictureIndex]).fadeOut(400, function() {
-					if(currentPictureIndex === gallery.length - 1){
-			currentPictureIndex = 0;
-		}
-			else{
-				currentPictureIndex++;
-			}
-		
-		$(gallery[currentPictureIndex]).slideDown(400);
-		})
+	image.on('click', function() {
+		current = $('img').index($(this));
+		container.empty();
+		container.append('<img src=\"' + image.eq(current).attr('src') + '\" />');
+		focus.fadeIn().addClass('enabled');
+		images.addClass('darken');
+		images.append('<div class="overlay"></div>');
+		$('html, body').animate({
+			scrollTop: focus.offset().top - 50
+		}, 500);
 	});
-})
+
+	close.on('click', function() {
+		focus.css('display', 'none').removeClass('enabled');
+		images.removeClass('darken');
+		$('.overlay').remove();
+	});
+});
