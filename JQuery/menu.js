92879bd3034b1b2ba68977441efeedb1cd30e495
Developoz <trung_vo_6@hotmail.com>
2016-01-19 14:23:41 +0100

    still need the close image button

--- JQuery/menu.js
+++ JQuery/menu.js
@@ -4,23 +4,18 @@ $(function() {
 	var current = -1;
 	var focus = $('.focus');
 	var container = $('.focus .container');
-	var close = $('.close');
 
-	image.on('click', function() {
-		current = $('img').index($(this));
-		container.empty();
-		container.append('<img src=\"' + image.eq(current).attr('src') + '\" />');
-		focus.fadeIn().addClass('enabled');
-		images.addClass('darken');
-		images.append('<div class="overlay"></div>');
-		$('html, body').animate({
-			scrollTop: focus.offset().top - 50
-		}, 500);
-	});
 
-	close.on('click', function() {
-		focus.css('display', 'none').removeClass('enabled');
-		images.removeClass('darken');
-		$('.overlay').remove();
+		image.on('click', function() {
+			current = $('img').index($(this));
+			container.empty();
+			container.append('<img src=\"' + image.eq(current).attr('src') + '\" />');
+			focus.fadeIn().addClass('enabled');
+			images.addClass('darken');
+			images.append('<div class="overlay"></div>');
+
 	});
+			image.on('click', function() {
+		
+});
 });
