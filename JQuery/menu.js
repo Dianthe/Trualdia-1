b6a785f2b012840d5927dbae056b484c342c5bf0
Developoz <trung_vo_6@hotmail.com>
2016-01-20 17:10:57 +0100

    gallery page

--- JQuery/menu.js
+++ JQuery/menu.js
@@ -1,12 +1,12 @@
 $(function() {
-	var images = $('.images');
-	var image = $('.images>img');
+	var images = $('.gallery');
+	var image = $('.gallery>.pic');
 	var focus = $('.focus');
 	var container = $('.focus .container');
 
 
 		image.on('click', function() {
-			current = $('img').index($(this));
+			current = $('.pic').index($(this));
 			container.empty();
 			container.append('<img src=\"' + image.eq(current).attr('src') + '\" />');
 			container.click(function() {
@@ -15,3 +15,24 @@ $(function() {
 			focus.fadeIn().addClass('enabled');
 	});
 });
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
